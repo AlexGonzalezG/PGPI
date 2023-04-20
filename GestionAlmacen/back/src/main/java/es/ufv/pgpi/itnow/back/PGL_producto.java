e5a187c209282a3adc8664321a9ce5f0a16bba17
unknown <alexggtems@outlook.es>
2023-04-20 18:34:48 +0200

    Creado el objeto con la estructura básica que tendrá un objeto en la base de datos

---
+++ GestionAlmacen/back/src/main/java/es/ufv/pgpi/itnow/back/PGL_producto.java
@@ -0,0 +1,46 @@
+package es.ufv.pgpi.itnow.back;
+
+public class  PGL_producto {
+
+    private String ID;
+    private String nombre_producto;
+    private int existencias;
+    private String ubicacion;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre_producto() {
+        return nombre_producto;
+    }
+
+    public void setNombre_producto(String nombre_producto) {
+        this.nombre_producto = nombre_producto;
+    }
+
+    public int getExistencias() {
+        return existencias;
+    }
+
+    public void setExistencias(int existencias) {
+        this.existencias = existencias;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+
+
+
+
+}
