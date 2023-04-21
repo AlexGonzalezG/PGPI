fda37b7d620674aef1cbab44cd7978dd0c5c3192
AlexGonzalezG <alexggtems@outlook.es>
2023-04-21 20:59:55 +0200

    Carga de datos de la BDD en memoria completa

--- GestionAlmacen/back/src/main/java/es/ufv/pgpi/itnow/back/PGL_productosBDD.java
+++ GestionAlmacen/back/src/main/java/es/ufv/pgpi/itnow/back/PGL_productosBDD.java
@@ -1,6 +1,36 @@
 package es.ufv.pgpi.itnow.back;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PGL_productosBDD {
-    // Esta clase va a coger todos los valores de la base de datos "PGL_BDDTemporal"
-    // y los va a cargar en memoria.
+    public static List<PGL_producto> cargarDatos() {
+        List<PGL_producto> productos = new ArrayList<>();
+        String url = "jdbc:sqlite:E:/Tablas/productos.db";
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            System.out.println("Connection to temporary database created successfully.");
+
+            Statement stmt = conn.createStatement();
+            String querySql = "SELECT * FROM productos";
+            ResultSet rs = stmt.executeQuery(querySql);
+
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                int existencias = rs.getInt("existencias");
+                String ubicacion = rs.getString("ubicacion");
+                float precio = rs.getFloat("precio");
+                productos.add(new PGL_producto(nombre, existencias, ubicacion, precio));
+            }
+
+            rs.close();
+            stmt.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return productos;
+    }
 }
