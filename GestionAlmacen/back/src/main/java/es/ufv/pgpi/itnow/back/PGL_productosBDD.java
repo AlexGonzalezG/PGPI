3b013ef713824771fbc2b50de5f58bb791b0df67
AlexGonzalezG <alexggtems@outlook.es>
2023-04-21 15:04:17 +0200

    Comentario del funcionamiento para la clase

--- GestionAlmacen/back/src/main/java/es/ufv/pgpi/itnow/back/PGL_productosBDD.java
+++ GestionAlmacen/back/src/main/java/es/ufv/pgpi/itnow/back/PGL_productosBDD.java
@@ -1,5 +1,6 @@
 package es.ufv.pgpi.itnow.back;
 
 public class PGL_productosBDD {
-
+    // Esta clase va a coger todos los valores de la base de datos "PGL_BDDTemporal"
+    // y los va a cargar en memoria.
 }
