110384273e5dd01dcddcfee5b9b53e46c2de05db
AlexGonzalezG <72098982+AlexGonzalezG@users.noreply.github.com>
2023-04-21 12:21:13 +0200

    Pongo esta feature en hold

--- GestionAlmacen/back/src/main/java/es/ufv/pgpi/itnow/back/PGL_encontrarProductoConPedido.java
+++ GestionAlmacen/back/src/main/java/es/ufv/pgpi/itnow/back/PGL_encontrarProductoConPedido.java
@@ -1,18 +1,23 @@
 package es.ufv.pgpi.itnow.back;
 
 public class PGL_encontrarProductoConPedido {
-    public static void pedidoEncontrado(String nombre){
+    public static PGL_producto pedidoEncontrado(PGL_pedido pedido){
+        /*
         PGL_producto producto = new PGL_producto();
         producto.setID("D34");
         producto.setNombre_producto("Crema");
         producto.setExistencias(40);
         producto.setUbicacion("A14");
         producto.setPrecio(40);
+        */
 
-        String nomprod = producto.getNombre_producto();
-        if (nomprod == nombre){
+        /*
+        nombre de producto (scrolldown)
+        cantidad solicitada (si no hay existencias, mensaje de "se pedirá al proveedor")
+        precio total
+         */
 
-        }
+
 
 
 
